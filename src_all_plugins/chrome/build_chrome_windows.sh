180df8dab1f0dbfab9d64bbaf97dc944725f8c46
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2024-07-13 14:26:09 -0400

    Add files via upload

---
+++ src_all_plugins/chrome/build_chrome_windows.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+mkdir WindowsBinaries
+
+TOP=$(pwd)  
+
+chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*geglchrome.dll') $TOP/WindowsBinaries
+
+TOP=$(pwd)  
+
+cd ..
+
+cd bevel && chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*bevel.dll') $TOP/WindowsBinaries
+
+cd .. 
+
+cd edge_smooth && chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*smoothedge.dll') $TOP/WindowsBinaries
+
+cd .. 
+
+cd threshold_alpha_2 && chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*thresholdalpha2.dll') $TOP/WindowsBinaries
+
+
+
+
+
