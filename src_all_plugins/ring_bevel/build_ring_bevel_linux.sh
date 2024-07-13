4cd8b55430eb8414d782e71fd7de74210fe49d48
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2024-07-13 14:23:03 -0400

    Add files via upload

---
+++ src_all_plugins/ring_bevel/build_ring_bevel_linux.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir LinuxBinaries
+
+TOP=$(pwd)  
+
+chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*ringbevel.so') $TOP/LinuxBinaries
+
+TOP=$(pwd)  
+
+cd ..
+
+cd ssg && chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*ssg.so') $TOP/LinuxBinaries
+
+cd .. 
+
+cd custom_bevel && chmod +x build_linux.sh
+
+./build_linux.sh
+
+cp $(find . -name '*cbevel.so') $TOP/LinuxBinaries
+
