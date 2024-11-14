74e85175cb5e7d4cf2808c47a47c0154d324aef1
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2024-11-13 22:25:38 -0500

    Update starburst.c

--- source_code_of_everything/starburst/starburst.c
+++ source_code_of_everything/starburst/starburst.c
@@ -180,7 +180,7 @@ gegl_op_class_init (GeglOpClass *klass)
   operation_class->attach = attach;
 
   gegl_operation_class_set_keys (operation_class,
-    "name",        "lb:starburst",
+    "name",        "sg:starburst",
     "title",       _("Starburst"),
     "categories",  "Artistic",
     "reference-hash", "45ed16360128agbr25493xc254001b2ac",
